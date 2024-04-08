810345e3f8e8466b0d2baa4b8355ef7e2792c0a5
DannerNiklas <20162002@atn.ac.at>
2024-04-08 18:00:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 ev3 = EV3Brick()
 
 # Initialize the motors
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+left_motor = Motor(Port.D)
+right_motor = Motor(Port.A)
 
 # Initialize the drive base
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
+rotation_angle = 180
+
 # Initialize the sensors
-left_color_sensor = ColorSensor(Port.S1)
-right_color_sensor = ColorSensor(Port.S2)
-front_ultrasonic_sensor = UltrasonicSensor(Port.S3)
-front_infrared_sensor = InfraredSensor(Port.S4)
+left_color_sensor = ColorSensor(Port.S3)
+right_color_sensor = ColorSensor(Port.S4)
+front_ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
 # Define the balloon color
 BALLOON_COLOR = Color.RED
 
-# Define the table edge color
-TABLE_EDGE_COLOR = Color.WHITE
+reflection_left = 10
+reflection_right = 10
 
 # Define the distance to the balloon (in mm)
 BALLOON_DISTANCE = 500
@@ -35,38 +36,33 @@ BALLOON_DISTANCE = 500
 # Define the distance to the balloon (in mm) for the color sensor
 COLOR_SENSOR_DISTANCE = 50
 
+isDriving = False
+
 def search_for_balloon():
+    isDriving = False
     while True:
         # Read the color sensors
         left_color = left_color_sensor.color()
         right_color = right_color_sensor.color()
 
-        # If either sensor detects the table edge, stop and turn
-        if left_color == TABLE_EDGE_COLOR or right_color == TABLE_EDGE_COLOR:
-            robot.stop()
-            wait(1000)
-            robot.straight(-100)  # Move backwards
-            robot.turn(90)  # Turn right
+
+        if (left_color_sensor.reflection() < reflection_left):
+            robot.stop
+            robot.turn(-rotation_angle)
+            continue
+        elif (right_color_sensor.reflection() < reflection_right):
+            robot.stop
+            robot.turn(rotation_angle)
+            isDriving = False
             continue
 
         # Read the ultrasonic sensor
         distance = front_ultrasonic_sensor.distance()
-
-        # If the balloon is detected, move towards it until it's within range of the color sensor
-        if distance < BALLOON_DISTANCE:
-            while distance > COLOR_SENSOR_DISTANCE:
-                robot.straight(100)  # Move forward
-                distance = front_ultrasonic_sensor.distance()
-
-            # Stop and check the balloon's color
-            robot.stop()
-            wait(1000)
-            color = front_color_sensor.color()
-            if color == BALLOON_COLOR:
-                ev3.speaker.beep()  # Found the balloon!
-                break
-
+        
+        
         # Otherwise, keep moving forward
-        robot.straight(100)
+        if isDriving == False:
+            robot.drive(100, 0)
+            isDriving = True
 
 search_for_balloon()
